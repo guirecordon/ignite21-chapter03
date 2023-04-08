1a6e85def8f48ffed657a098d861614c6c8ce3de
Gui Recordon <guilherme.recordon@gmail.com>
2023-04-08 18:52:05 -0300

    Add styling of main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head'
 
+import styles from './styles.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | Ig news</title>
       </Head>
-      <h1>Hello, world</h1>
+      <main className={styles.mainContainer}>
+        <div className={styles.leftSideHero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>News about
+            the <span>React</span> world
+          </h1>
+
+          <p>Get acess to all the publications
+            <strong>for {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(9.90)} month</strong>
+          </p>
+        </div>
+
+        <img src="/images/heroimg.svg" alt="girl on computer" />
+      </main>
     </>
   )
 }
