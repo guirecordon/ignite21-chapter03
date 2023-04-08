a0c456b2b383c4c1669e953934ed00a7b00c1ce4
Gui Recordon <guilherme.recordon@gmail.com>
2023-04-08 15:58:24 -0300

    Finish basic setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello, world</h1>
+    <>
+      <Head>
+        <title>Início | Ig news</title>
+      </Head>
+      <h1>Hello, world</h1>
+    </>
   )
 }
