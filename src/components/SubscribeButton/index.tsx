2f9418f4878ba0ef9ee146d3e0003a04ea444d25
Gui Recordon <guilherme.recordon@gmail.com>
2023-04-12 13:05:32 -0300

    Add rule to avoid customer duplication on stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { api } from '@/services/api';
+import { getStripeJs } from '@/services/stripe-js';
 import { useSession, signIn } from 'next-auth/react'
 
 import styles from './styles.module.scss';
@@ -9,13 +11,27 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ productId }: SubscribeButtonProps) {
   const { status } = useSession()
   
-  function handleSubscription() {
+  async function handleSubscription() {
     if(status === 'unauthenticated') {
+      alert('You\'ll be redirect to sign in before you can subscribe. Please try again once you\'re signed in')
       signIn('github')
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch(error) {
+      if(error instanceof Error) {
+        alert(error.message)
+      }
+      alert('An error ocurred while trying to create a subscription')
+    }
   }
   
   
