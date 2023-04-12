e503fd3daa83cebbc3560857a1791be797ab1ded
Gui Recordon <guilherme.recordon@gmail.com>
2023-04-11 21:59:01 -0300

    Fix ts next-auth and i faunadb

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      scope: 'read:user'
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
