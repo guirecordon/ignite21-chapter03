fef3e8c99b7ad6c4cb06b449e2d9f8f9ddf844b4
Gui Recordon <guilherme.recordon@gmail.com>
2023-04-11 23:11:24 -0300

    Add fauna queries to check duble user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query } from "faunadb"
+import { fauna } from "@/services/fauna"
+
+type userType = {
+  user: {
+    email: string;
+  }
+}
 
 export const authOptions = {
   providers: [
@@ -9,6 +17,40 @@ export const authOptions = {
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn({ user }: any) {
+      const { email } = user;
+      
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 }
 
 export default NextAuth(authOptions)
