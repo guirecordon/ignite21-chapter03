a03b43b8bd63bee0036b48dfbf18b87fc323b08d
Gui Recordon <guilherme.recordon@gmail.com>
2023-04-14 18:42:51 -0300

    Add prismic

---
+++ .slicemachine/assets/slices/Test/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Test';
+
+export default {
+  title: 'slices/Test'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Orange","spans":[]}],"description":[{"type":"paragraph","text":"Ad incididunt consequat sit reprehenderit ipsum labore veniam consectetur veniam.","spans":[]}]},"id":"_Default","slice_type":"test"}} />
+_Default.storyName = ''
